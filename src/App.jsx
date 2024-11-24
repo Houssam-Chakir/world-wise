cee6ba321b983b9a7c475309627547e1ddc7a8c6
houssam chakir <thisischchannel@gmail.com>
2024-11-24 20:04:57 +0100

    added react router and created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/pricing";
+import NotFound from "./pages/NotFound";
+
+
 function App() {
-  const x = 0;
-  return <div>worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

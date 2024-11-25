05cdde37ae1d1389ad9a73fc6175556d5cb3c11c
houssam chakir <thisischchannel@gmail.com>
2024-11-25 22:36:20 +0100

    implimented routes and nested routes for navigation using react router, and styles uising css modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/pricing";
 import NotFound from "./pages/NotFound";
-
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route index element={<Homepage />} />
+        <Route path='app' element={<AppLayout />}>
+          <Route index element={<p>cities</p>} />
+          <Route path='cities' element={<p>cities</p>} />
+          <Route path='countries' element={<p>countries</p>} />
+          <Route path='form' element={<p>form</p>} />
+        </Route>
+        <Route path='login' element={<Login />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path='*' element={<NotFound />} />

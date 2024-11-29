13bdb6352ddb45ba48a9f740f97d5f3deec09335
houssam chakir <thisischchannel@gmail.com>
2024-11-30 00:35:32 +0100

    added functional map to the app (leaflet), modified no unused var rule to warn in eslintrc.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,29 @@
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import styles from './Map.module.css'
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 function Map() {
-  const navigateTo = useNavigate()
+  const navigateTo = useNavigate();
 
-  const [searchParams, setSearchParams] = useSearchParams()
-  const lat = searchParams.get('lat')
-  const lng = searchParams.get('lng')
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  // eslint-disable-next-line no-unused-vars
+  const [mapPosition, setMapPosition] = useState([40, 0])
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigateTo('form')}>
-      <h1>Map</h1>
-      <p>Position: {lat}, {lng}</p>
-      <button onClick={() => setSearchParams({lat: 22, lng: 23})}>Change</button>
+    <div className={styles.mapContainer}>
+      <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={false}>
+        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;

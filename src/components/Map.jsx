2cb81b2ca4cd74561b70a811ff07540ad5141804
houssam chakir <thisischchannel@gmail.com>
2024-11-30 19:31:32 +0100

    Clicking on map navigates you to Form. Todo: pre fill data about clicked location to form. Fixed: location no longer resets after going back from selected country but stays on it until next country selected.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,28 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../context/CitiesContext";
 
 function Map() {
-  const { cities } = useCities();
   const navigateTo = useNavigate();
-
+  const { cities } = useCities();
   const [searchParams, setSearchParams] = useSearchParams();
+
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  const [mapPosition, setMapPosition] = useState(cities[0].position);
+  useEffect(
+    function () {
+      if (lat && lng) setMapPosition([lat, lng]);
+    },
+    [lat, lng, setMapPosition]
+  );
 
   return (
     <div className={styles.mapContainer}>
-      <MapContainer className={styles.map} center={mapPosition} zoom={8} scrollWheelZoom={true}>
+      <MapContainer className={styles.map} center={mapPosition} zoom={6} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -25,14 +31,33 @@ function Map() {
           return (
             <Marker key={city.id} position={city.position}>
               <Popup>
-                <span>{city.emoji}</span><span>{city.cityName}</span>
+                <span>{city.emoji}</span>
+                <span>{city.cityName}</span>
               </Popup>
             </Marker>
           );
         })}
+        <ChangeView position={mapPosition} />
+        <DetectClick navigateTo={navigateTo} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeView({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick({navigateTo, cityName}) {
+  const map = useMapEvents({
+    click: (e) => {
+      console.log('e: ', e);
+      navigateTo(`form?city=${cityName}&lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+    }
+  })
+  return null
 }
 
 export default Map;

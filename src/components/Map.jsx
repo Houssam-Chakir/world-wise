e24ce14fed6b9e12384556f8ba7e25cf0285502e
houssam chakir <thisischchannel@gmail.com>
2024-11-30 12:47:47 +0100

    display cities markers on Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,25 +2,34 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../context/CitiesContext";
 
 function Map() {
+  const { cities } = useCities();
   const navigateTo = useNavigate();
 
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  // eslint-disable-next-line no-unused-vars
-  const [mapPosition, setMapPosition] = useState([40, 0])
+
+  const [mapPosition, setMapPosition] = useState(cities[0].position);
 
   return (
     <div className={styles.mapContainer}>
-      <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={false}>
-        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+      <MapContainer className={styles.map} center={mapPosition} zoom={8} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        {cities.map((city) => {
+          return (
+            <Marker key={city.id} position={city.position}>
+              <Popup>
+                <span>{city.emoji}</span><span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
